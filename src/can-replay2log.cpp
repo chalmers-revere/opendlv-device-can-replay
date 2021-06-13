8c435c0da55b2b0606240234aa851067eaee9775
Christian Berger <christian.berger@gu.se>
2021-06-13 20:22:25 +0200

    * added --asc and --log support
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- src/can-replay2log.cpp
+++ src/can-replay2log.cpp
@@ -21,24 +21,28 @@
 #include <unistd.h>
 
 #include <cstring>
-
+#include <ctime>
 #include <iostream>
 #include <iomanip>
+#include <locale>
 #include <string>
 
 int32_t main(int32_t argc, char **argv) {
     int32_t retCode{1};
     const std::string PROGRAM{argv[0]}; // NOLINT
     auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
-    if ( (0 == commandlineArguments.count("can")) ) {
-        std::cerr << argv[0] << " converts captured raw CAN frames in opendlv.proxy.RawUInt64CANFrame format into .asc format with the specified CAN device as prefix." << std::endl;
-        std::cerr << "Usage:   " << argv[0] << " [--id=ID] --can=<name of the CAN device>" << std::endl;
-        std::cerr << "         --id:     ID of opendlv.proxy.RawCANFrame to replay; default: 0" << std::endl;
-        std::cerr << "Example: " << argv[0] << " --can=vcan0" << std::endl;
+    if ( (0 == commandlineArguments.count("log")) && (0 == commandlineArguments.count("asc")) ) {
+        std::cerr << argv[0] << " converts captured raw CAN frames in opendlv.proxy.RawUInt64CANFrame format into .asc or .log format with the specified CAN device as prefix." << std::endl;
+        std::cerr << "Usage:   " << argv[0] << " [--id=ID] --log" << std::endl;
+        std::cerr << "         --asc:    convert RawUInt64CANFrame into .asc format" << std::endl;
+        std::cerr << "         --log:    convert RawUInt64CANFrame into .log format" << std::endl;
+        std::cerr << "         --id:     ID of opendlv.proxy.RawCANFrame to replay; default: -1 to export all CAN frames" << std::endl;
+        std::cerr << "Example: " << argv[0] << " --log" << std::endl;
     }
     else {
-        const std::string CANDEVICE{commandlineArguments["can"]};
-        const uint32_t ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+        const int32_t ID{(commandlineArguments["id"].size() != 0) ? static_cast<int32_t>(std::stoi(commandlineArguments["id"])) : -1};
+        const bool ASC{(commandlineArguments["asc"].size() != 0)};
+        const bool LOG{(commandlineArguments["log"].size() != 0)};
 
         std::string recFile;
         for (auto e : commandlineArguments) {
@@ -58,12 +62,23 @@ int32_t main(int32_t argc, char **argv) {
             constexpr bool THREADING{false};
             cluon::Player player(recFile, AUTOREWIND, THREADING);
 
+            cluon::data::TimeStamp first;
             while (player.hasMoreData() && !cluon::TerminateHandler::instance().isTerminated.load()) {
                 auto next = player.getNextEnvelopeToBeReplayed();
                 if (next.first) {
                     cluon::data::Envelope e = next.second;
 
-                    if ( (e.dataType() == opendlv::proxy::RawUInt64CANFrame::ID()) && (e.senderStamp() == ID) ) {
+                    if ( (e.dataType() == opendlv::proxy::RawUInt64CANFrame::ID()) && ((-1 == ID) || (static_cast<int32_t>(e.senderStamp()) == ID)) ) {
+                        if ( 0 == (first.seconds() + first.microseconds()) ) {
+                            first = e.sampleTimeStamp();
+                            if (ASC) {
+                                std::time_t t = static_cast<std::time_t>(first.seconds());
+                                char mbstr[100];
+                                if (std::strftime(mbstr, sizeof(mbstr), "%a %b %d %r %G", std::localtime(&t))) {
+                                    std::cout << "Begin Triggerblock " << mbstr << std::endl;
+                                }
+                            }
+                        }
                         canFrame = cluon::extractMessage<opendlv::proxy::RawUInt64CANFrame>(std::move(e));
                         if (0 < canFrame.length()) {
                             union CANData {
@@ -71,20 +86,38 @@ int32_t main(int32_t argc, char **argv) {
                                 uint64_t value{0};
                             } canData;
                             canData.value = canFrame.data();
-                            std::cout << "(" << e.sampleTimeStamp().seconds() << "."
-                                      << std::setfill('0') << std::setw(6) << e.sampleTimeStamp().microseconds() << ")" 
-                                      << " " << CANDEVICE << " ";
-                            std::cout << std::setfill('0') << std::setw(0) << std::uppercase
-                                      << std::hex << (canFrame.canID() & 0x3FFFFFFF)
-                                      << "#";
-                            std::cout << std::setfill('0') << std::setw(2) << std::uppercase;
-                            for(uint8_t i{0}; i<canFrame.length(); i++) {
-                                std::cout << std::hex << (+((uint8_t)canData.bytes[i]));
+                            if (LOG) { 
+                                std::cout << "(" << e.sampleTimeStamp().seconds() << "."
+                                          << std::setfill('0') << std::setw(6) << e.sampleTimeStamp().microseconds() << ")" 
+                                          << " can" << e.senderStamp() << " ";
+                                std::cout << std::setfill('0') << std::setw(0) << std::uppercase
+                                          << std::hex << (canFrame.canID() & 0x3FFFFFFF)
+                                          << "#";
+                                std::cout << std::setfill('0') << std::setw(2) << std::uppercase;
+                                for(uint8_t i{0}; i<canFrame.length(); i++) {
+                                    std::cout << std::hex << std::setfill('0') <<  std::setw(2) << (+((uint8_t)canData.bytes[i]));
+                                }
+                                std::cout << std::dec << std::endl;
                             }
-                            std::cout << std::dec << std::endl;
+                            if (ASC) {
+                                std::cout << "   " << (e.sampleTimeStamp().seconds() - first.seconds()) << "."
+                                          << std::setfill('0') << std::setw(6) << e.sampleTimeStamp().microseconds() << " " 
+                                          << " " << e.senderStamp() << "  ";
+                                std::cout << std::setfill('0') << std::setw(0) << std::uppercase
+                                          << std::hex << (canFrame.canID() & 0x3FFFFFFF) << "x"
+                                          << "\t\tRx d " << std::dec << +(canFrame.length());
+                                std::cout << std::setfill('0') << std::setw(2) << std::uppercase;
+                                for(uint8_t i{0}; i<canFrame.length(); i++) {
+                                    std::cout << " " << std::hex << std::setfill('0') <<  std::setw(2) << (+((uint8_t)canData.bytes[i]));
+                                }
+                                std::cout << std::dec << std::endl;
+                            }
                         }
                     }
                 }
+            }
+            if (ASC) {
+                std::cout << "End TriggerBlock" << std::endl;
             }
             retCode = 0;
         }
